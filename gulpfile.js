698dc5afa1cd2c60bddc24b9d9ddb9ebc55b34d9
Artur Trifonov <artur@uniteddesign.ru>
2018-03-31 16:13:29 +0300

    gulp installed

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    livereload = require('gulp-livereload');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+        .pipe(livereload());
+});
+
+gulp.task('sass:watch', function () {
+    livereload.listen();
+    gulp.watch(['./sass/**/*.scss', './sass/*/*.scss'], ['sass']);
+});
